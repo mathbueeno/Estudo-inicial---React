d57fafcbf489c9ba4b2d40173e9d846c8a7f0d50
MatheusAbreuBueno <matheus.bueno@hubcount.com.br>
2024-04-18 10:02:55 -0300

    Implementação do app-content

--- src/app.js
+++ src/app.js
@@ -1,44 +1,38 @@
 'use strict' // o que é ?
 import React, { Component } from 'react' // Sempre importar usando o shorthand notation
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+import AppContent from './components/app-content'
 
+// Componente Principal
+// o App é Statefull (smartcomponent) - único que será o inteligente
+// Informações de estado - somente no app, Container component - serve pra fazer todas as ações. (ex: request)
+class App  extends Component {
+    constructor() {
+        super()
+        this.state = {
+            userinfo: {
+                username: 'Matheus Bueno',
+                repos: 28,
+                followers: 36,
+                following: 64
+            },
+            repos: [{
+                name: 'Repo',
+                link: '#'
+            }],
+            starred: [{
+                name: 'Repo',
+                link: '#'
+            }]
+        }
+    }
 
-const App = () => (
-
-    // Quebrando em componentes
-
-    <div className='app'>
-    
-        {/* Barra de pesquisa */}
-        < Search />
-
-        {/* Informações do Usuário */}
-        < UserInfo/>
-
-        {/* Ações dos Botões     */}
-        <Actions/>
-
-        {/* Repositórios */}
-        <Repos 
-        className='repos' 
-        title='Repositórios:'
-        repos = {[{
-            name: 'nome do repositório',
-            link: '#'
-        }]}/>  
-
-
-        <Repos 
-        className='starred' 
-        title='Favoritos:'
-        repos = {[{
-            name: 'Nome do repositório',
-            link: '#'
-        }]}/>     
-    </div>
-)
-
+    render(){
+        return <AppContent
+        userinfo = {this.state.userinfo}
+        repos = {this.state.repos}
+        starred={this.state.starred}
+        />
+        
+    }
+}
 export default App
---
+++ src/components/app-content.js
@@ -0,0 +1,46 @@
+'use strict'
+import React, {PropTypes} from "react"
+import Search from './search'
+import UserInfo from './user-info'
+import Actions from './actions'
+import Repos from './repos'
+
+const AppContent = ({userinfo, repos, starred}) => (
+    // Quebrando em componentes
+
+    <div className='app'>
+    
+    {/* Barra de pesquisa */}
+        < Search />
+
+    {/* Informações do Usuário */}
+    {/* Curto Circuito do JS */}
+        {!!userinfo && < UserInfo userinfo={userinfo}/>}
+
+    {/* Ações dos Botões     */}
+        {!!userinfo && <Actions/>}
+
+    {/* Repositórios */}
+        {!!repos.length &&
+        <Repos 
+            className='repos' 
+            title='Repositórios:'
+            repos = {repos}
+        />
+    }  
+        {!!starred.length &&
+        <Repos 
+            className='starred' 
+            title='Favoritos:'
+            repos = {starred}
+        />
+    }     
+    </div>
+
+)
+AppContent.propTypes = {
+    userinfo:PropTypes.object, 
+    repos:PropTypes.array.isRequired,
+    starred:PropTypes.array.isRequired
+}
+export default AppContent
